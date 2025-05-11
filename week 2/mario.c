0c19de232678fa212f4d87aa26a844cb27ac4bd9
Nakib Noor <84078873+tahfimism@users.noreply.github.com>
2025-05-11 17:02:33 +0600

    Create mario.c

---
+++ week 2/mario.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void print_row(int n);
+
+int main (void)
+{
+
+    int n= 3;
+    for ( int row=0; row < n; row++)
+    {
+       print_row(n);
+    }
+
+
+
+}
+
+
+void print_row(int n)
+{
+    for (int r=0; r < 3; r++)
+        {
+            printf("#");
+        }
+        printf("\n");
+}
