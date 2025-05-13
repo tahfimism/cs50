402120671e2db91ca5bc5be8a03e9681faf89629
Nakib Noor <84078873+tahfimism@users.noreply.github.com>
2025-05-13 21:47:39 +0600

    Create phonebook.c

---
+++ week 4/phonebook.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    // opening the phonebook
+    FILE *book = fopen("phonebook.csv", "a");
+
+    // to ask if we want to put another contact
+    char repeat = 'y';
+
+    while (repeat == 'y')
+    {
+        // getting inputs
+        char *name = get_string("name: ");
+        char *number = get_string("number: ");
+
+        // printing the contact
+        fprintf(book, "%s, %s\n", name, number);
+
+        // asking if we wanna put another contact
+        repeat = get_char("another? y/n ");
+    }
+
+    fclose(book);
+}
