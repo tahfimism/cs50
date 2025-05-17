2cb424aa552cffe8e4b048e2b09e28c08bc43e49
Nakib Noor <84078873+tahfimism@users.noreply.github.com>
2025-05-17 19:22:21 +0600

    Create atoi.c

---
+++ week 3/practice-problem/atoi.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int convert(string input);
+
+int main(void)
+{
+    string input = get_string("Enter a positive integer: ");
+
+    for (int i = 0, n = strlen(input); i < n; i++)
+    {
+        if (!isdigit(input[i]))
+        {
+            printf("Invalid Input!\n");
+            return 1;
+        }
+    }
+
+    // Convert string to int
+    printf("%i\n", convert(input));
+}
+
+int convert(string input)
+{
+    // TODO
+    int n = strlen(input);
+    int number = 0;
+    int power = n-1;
+
+    for(int i = 0 ; i < n; i++)
+    {
+        number = number + (input[i] - '0') * pow(10,power);
+        power--;
+    }
+
+    return number;
+}
